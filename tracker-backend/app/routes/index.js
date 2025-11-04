b45a988db9eb8b04bb8e44630ec5ba0130dea6f4
CynthiaNirere <cynthia.nirere@eagles.oc.edu>
2025-11-04 08:38:23 -0600

    worked on index

--- tracker-backend/app/routes/index.js
+++ tracker-backend/app/routes/index.js
@@ -3,7 +3,6 @@ import { Router } from "express";
 import AuthRoutes from "./auth.routes.js";
 import UserRoutes from "./user.routes.js";
 import TutorialRoutes from "./tutorial.routes.js";
-import LessonRoutes from "./lesson.routes.js";
 
 
 const router = Router();
@@ -11,6 +10,5 @@ const router = Router();
 router.use("/", AuthRoutes);
 router.use("/users", UserRoutes);
 router.use("/tutorials", TutorialRoutes);
-router.use("/tutorials", LessonRoutes);
 
 export default router;
